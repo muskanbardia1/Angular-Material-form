65de09d78b3fe2677c2ae11bfddbfe4a004679a8
unknown <muskanbardia98@gmail.com>
2021-04-26 15:26:01 +0530

    Angular-Material-Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MaterialModule } from './material.module';
+import { ReactiveFormsModule } from '@angular/forms'
 
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { from } from 'rxjs';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+},{
+  path: 'register',
+  component: RegisterComponent
+},
+{
+  path: 'login',
+  component: LoginComponent
+}]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
+    , MaterialModule,
+    ReactiveFormsModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/material.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatToolbarModule
+  
+} from '@angular/material/toolbar';
+import { MatButtonModule} from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  imports:[
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule
+
+  ]
+})
+
+export class MaterialModule { }
